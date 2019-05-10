9408ae75a6735716712012a1202f59f5ff1a85fb
Diksi Erlangga <diksi131@gmail.com>
2019-05-10 14:45:22 +0800

    Ganti Nama
    pean.php jadi pesan.php, tambah form isi pesan

---
+++ pesan.php
@@ -0,0 +1,8 @@
+<html>
+    <form action="prosespesan.php" method="post">
+        <label>Masukkan Pesan Anda</label>
+        <textarea name="isipesan"></textarea>
+
+        <input type="submit">
+    </form>
+</html>
