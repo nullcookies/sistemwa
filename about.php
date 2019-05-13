3835ea78ce96755d0ffee38b8cc5bba561637b8c
rizky mochtar <mochtarbuchori39@gmail.com>
2019-05-13 08:12:07 +0700

    About

---
+++ about.php
@@ -0,0 +1,7 @@
+<h1>About Program</h1>
+<p>Program Auto Send Massage Via Whatsapp yang memiliki sistem scheduling<br>
+    untuk memudahkan pengiriman pesan
+    dan juga menghindari blokir akun atau spam<br>
+    <b>&copy;Copyright 2019 All Right Reserved</b>
+</p>
+<hr>
